d53c1360750007e9fe2b8e95ecc87971099512cb
samperay <sunlnx@gmail.com>
2020-01-26 15:33:33 +0530

    docker tutorial added

---
+++ docker-files/tutorial/README.md
@@ -0,0 +1,3 @@
+# docker
+
+more tutorial on this link - http://bit.ly/3158oUh
---
+++ docker-files/tutorial/run.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+echo "The current directory : $(pwd)"
+echo "The DZ variable : $DZ"
+echo "There are $# arguments: $@"
